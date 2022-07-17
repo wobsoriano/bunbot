6cc788d458ac2f3ac4c962a2b58019813e7d1260
Robert Soriano <sorianorobertc@gmail.com>
2022-07-17 09:51:36 -0700

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 
-CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 CC=~/go/bin/zigcc CXX=~/go/bin/zigcpp go build --buildmode c-shared -o ./release/darwin-amd64 -ldflags "-s -w" main.go
-CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 CC=~/go/bin/zigcc CXX=~/go/bin/zigcpp go build --buildmode c-shared -o ./release/darwin-arm64 -ldflags "-s -w" main.go
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=~/go/bin/zigcc CXX=~/go/bin/zigcpp go build --buildmode c-shared -o ./release/windows-amd64 -ldflags "-s -w -H=windowsgui" main.go
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=~/go/bin/zigcc CXX=~/go/bin/zigcpp go build --buildmode c-shared -o ./release/linux-amd64 -ldflags "-s -w" main.go
-CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CC=~/go/bin/zigcc CXX=~/go/bin/zigcpp go build --buildmode c-shared -o ./release/linux-arm64 -ldflags "-s -w" main.go
+CC=~/go/bin/zigcc
+CXX=~/go/bin/zigcpp
+CGO_ENABLED=1
+FILENAME=main.go
+
+CGO_ENABLED=$CGO_ENABLED GOOS=darwin GOARCH=amd64 CC=$CC CXX=$CXX go build --buildmode c-shared -o ./release/darwin-amd64 -ldflags "-s -w" $FILENAME
+CGO_ENABLED=$CGO_ENABLED GOOS=darwin GOARCH=arm64 CC=$CC CXX=$CXX go build --buildmode c-shared -o ./release/darwin-arm64 -ldflags "-s -w" $FILENAME
+CGO_ENABLED=$CGO_ENABLED GOOS=windows GOARCH=amd64 CC=$CC CXX=$CXX go build --buildmode c-shared -o ./release/windows-amd64 -ldflags "-s -w -H=windowsgui" $FILENAME
+CGO_ENABLED=$CGO_ENABLED GOOS=linux GOARCH=amd64 CC=$CC CXX=$CXX go build --buildmode c-shared -o ./release/linux-amd64 -ldflags "-s -w" $FILENAME
+CGO_ENABLED=$CGO_ENABLED GOOS=linux GOARCH=arm64 CC=$CC CXX=$CXX go build --buildmode c-shared -o ./release/linux-arm64 -ldflags "-s -w" $FILENAME
