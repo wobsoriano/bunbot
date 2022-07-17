8e6b104c1e473d19e4a464664f88204282fff45f
Robert Soriano <sorianorobertc@gmail.com>
2022-07-16 19:10:23 -0700

    refactor and add utils

--- src/ffi.ts
+++ src/ffi.ts
@@ -1,15 +1,5 @@
-import { dlopen, FFIType, suffix, CString } from 'bun:ffi'
-
-// const utf8e = new TextEncoder()
-
-// function encode<T>(data: T): Uint8Array {
-//   return utf8e.encode(data + "\0")
-// }
-
-export type Signal = {
-  signal: 'CtrlC' | 'CtrlD' | null
-  value: string | null
-}
+import { dlopen, FFIType, suffix } from 'bun:ffi'
+import { toString } from './utils'
 
 const { symbols } = dlopen(`${import.meta.dir}/../release/bunbot.${suffix}`, {
   GetVersion: {
@@ -33,14 +23,10 @@ export function freeString(ptr: number) {
 
 export function getVersion(): string {
   const ptr = symbols.GetVersion()
-  const str = new CString(ptr)
-  freeString(str.ptr)
-  return str.toString()
+  return toString(ptr)
 }
 
 export function getMouseColor(): string {
   const ptr = symbols.GetMouseColor()
-  const str = new CString(ptr)
-  freeString(str.ptr)
-  return str.toString()
+  return toString(ptr)
 }
