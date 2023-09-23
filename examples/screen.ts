c0f8861c1699c129fb38041a069521cbb001e8b7
wobsoriano <sorianorobertc@gmail.com>
2023-09-22 21:04:22 -0700

    update playground

--- examples/screen.ts
+++ examples/screen.ts
@@ -3,22 +3,17 @@ import { CreateBot } from '../src'
 const bot = CreateBot()
 
 setTimeout(() => {
-  const bit = bot.captureScreen(10, 10, 30, 30)
-  bot.freeBitmap(bit)
-  // const img = bot.toImage(bit)
-  // console.log(img)
+  const num = bot.displaysNum()
 
-  // const num = bot.displaysNum()
-  
-  // for (let i = 0; i < num; i++) {
-  //   bot.setDisplayID(i)
+  for (let i = 0; i < num; i++) {
+    bot.setDisplayID(i)
 
-  //   const img1 = bot.captureImg()
-  //   const path1 = `save_${i}`
-  //   bot.save(img1, `${path1}.png`)
-  //   bot.saveJpeg(img1, `${path1}.jpeg`, 50)
+    const img1 = bot.captureImg()
+    const path1 = `save_${i}`
+    bot.save(img1, `${path1}.png`)
+    bot.saveJpeg(img1, `${path1}.jpeg`, 50)
 
-  //   const img2 = bot.captureImg(10, 10, 20, 20)
-  //   bot.save(img2, `test_${i}.png`)
-  // }
+    const img2 = bot.captureImg(10, 10, 20, 20)
+    bot.save(img2, `test_${i}.png`)
+  }
 }, 1000)
